6e3e8b7a7067ab1540651d70ddd04275401c640e
Bekione <kinber024@gmail.com>
2023-07-24 13:08:30 +0300

    Change method to arrow function

--- index.js
+++ index.js
@@ -3,16 +3,9 @@ import { BouncingDVDLogo } from "./Module/BouncingDVDLogo.js";
 const container = document.getElementById('container');
 const bouncingDVDLogo = new BouncingDVDLogo(container, 100, 50, 2)
 
-// const animate = () => {
-//     bouncingDVDLogo.move();
-//     window.requestAnimationFrame(animate())
-// }
-
-// animate();
-
-function animate() {
+const animate = () => {
     bouncingDVDLogo.move();
-    requestAnimationFrame(animate);
+    window.requestAnimationFrame(animate)
 }
 
 animate();
